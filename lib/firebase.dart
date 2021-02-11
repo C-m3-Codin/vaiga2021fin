3462dd64d206eaa97ea69da8c4e0bd0c2339a169
you@_simon <sonuisplaying@gmail.com>
2021-02-11 11:18:23 +0530

    readFromRTDB

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -2,9 +2,18 @@
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 Future initializeFirebaseApp(BuildContext context) async {
   await Firebase.initializeApp().then((_) {
     print('Firebase gets initialized');
+    readFromRTDB();
+  });
+}
+
+Future readFromRTDB() {
+  DatabaseReference databaseReference = FirebaseDatabase.instance.reference();
+  databaseReference.once().then((DataSnapshot datasnapshot) {
+    print(datasnapshot.value['test string']);
   });
 }
