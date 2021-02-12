6849723e546d53aa59df6839b7ac18dcb107139c
cp <cyril199897@gmail.com>
2021-02-12 07:42:58 +0530

    Node done

--- lib/screens/Detail.dart
+++ lib/screens/Detail.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vaiga_farmcare/models/StreamParser.dart';
 // import 'package:flutter/widgets.dart';
 // import 'package:flutter/material.dart';
 // import 'package:plant_app/screens/details/components/body.dart';
@@ -29,26 +31,26 @@ class Body extends StatelessWidget {
           SizedBox(height: kDefaultPadding),
           Row(
             children: <Widget>[
-              SizedBox(
-                width: size.width / 2,
-                height: 84,
-                child: FlatButton(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(20),
-                    ),
-                  ),
-                  color: kPrimaryColor,
-                  onPressed: () {},
-                  child: Text(
-                    "Buy Now",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
+              // SizedBox(
+              //   width: size.width / 2,
+              //   height: 84,
+              //   child: FlatButton(
+              //     shape: RoundedRectangleBorder(
+              //       borderRadius: BorderRadius.only(
+              //         topRight: Radius.circular(20),
+              //       ),
+              //     ),
+              //     color: kPrimaryColor,
+              //     onPressed: () {},
+              //     child: Text(
+              //       "Buy Now",
+              //       style: TextStyle(
+              //         color: Colors.white,
+              //         fontSize: 16,
+              //       ),
+              //     ),
+              //   ),
+              // ),
               Expanded(
                 child: FlatButton(
                   onPressed: () {},
@@ -69,7 +71,7 @@ class Body extends StatelessWidget {
 // import '../../../constants.dart';
 // import 'icon_card.dart';
 
-class ImageAndIcons extends StatelessWidget {
+class ImageAndIcons extends StatefulWidget {
   const ImageAndIcons({
     Key key,
     @required this.size,
@@ -78,113 +80,138 @@ class ImageAndIcons extends StatelessWidget {
   final Size size;
 
   @override
+  _ImageAndIconsState createState() => _ImageAndIconsState();
+}
+
+class _ImageAndIconsState extends State<ImageAndIcons> {
+  final databaseRef = FirebaseDatabase.instance.reference();
+  DatabaseReference _dhtref = FirebaseDatabase.instance.reference().child('');
+
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
-      child: SizedBox(
-        height: size.height * 0.8,
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: kDefaultPadding * 3),
-                child: Column(
+    return StreamBuilder(
+        stream: _dhtref.onValue,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && !snapshot.hasError) {
+            var _dht = DHT.fromJson(snapshot.data.snapshot.value['Node1']);
+            print("DHT: ${_dht.solidTEmp} / ${_dht.humidity} / ${_dht.temp}");
+            return Padding(
+              padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+              child: SizedBox(
+                height: widget.size.height * 0.8,
+                child: Row(
                   children: <Widget>[
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: IconButton(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: kDefaultPadding * 3),
+                        child: Column(
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: IconButton(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: kDefaultPadding),
+                                icon: SvgPicture.asset(
+                                    "assets/icons/back_arrow.svg"),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            Spacer(),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconCard(icon: "assets/icons/sun.svg"),
+                                  Text(
+                                    '125 lux',
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconCard(icon: "assets/icons/icon_4.svg"),
+                                  Text(
+                                    _dht.smoistNow.toString(),
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 12.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconCard(icon: "assets/icons/icon_3.svg"),
+                                  Text(
+                                    _dht.humidity.toString(),
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconCard(icon: "assets/icons/icon_2.svg"),
+                                  Text(
+                                    _dht.temp.toString(),
+                                    style: TextStyle(fontSize: 18),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconCard(icon: "assets/icons/sun.svg"),
-                          Text(
-                            '125 lux',
-                            style: TextStyle(fontSize: 18),
-                          )
+                    Container(
+                      height: widget.size.height * 0.7,
+                      width: widget.size.width * 0.50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(63),
+                          bottomLeft: Radius.circular(63),
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 10),
+                            blurRadius: 60,
+                            color: kPrimaryColor.withOpacity(0.29),
+                          ),
                         ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconCard(icon: "assets/icons/icon_4.svg"),
-                          Text(
-                            '125 lux',
-                            style: TextStyle(fontSize: 18),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconCard(icon: "assets/icons/icon_3.svg"),
-                          Text(
-                            '125 lux',
-                            style: TextStyle(fontSize: 18),
-                          )
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconCard(icon: "assets/icons/icon_2.svg"),
-                          Text(
-                            '125 lux',
-                            style: TextStyle(fontSize: 18),
-                          )
-                        ],
+                        image: DecorationImage(
+                          alignment: Alignment.centerLeft,
+                          fit: BoxFit.cover,
+                          image: AssetImage("assets/images/img.png"),
+                        ),
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-            Container(
-              height: size.height * 0.7,
-              width: size.width * 0.65,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(63),
-                  bottomLeft: Radius.circular(63),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 60,
-                    color: kPrimaryColor.withOpacity(0.29),
-                  ),
-                ],
-                image: DecorationImage(
-                  alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/images/img.png"),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+            );
+          }
+        });
   }
 }
 
@@ -273,13 +300,6 @@ class TitleAndPrice extends StatelessWidget {
             ),
           ),
           Spacer(),
-          Text(
-            "\$$price",
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                .copyWith(color: kPrimaryColor),
-          )
         ],
       ),
     );
